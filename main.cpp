1bd6201ac018e4704bff5cbeac02e4cda0d5eb2c
Mdub28 <weiguang2828@gmail.com>
2014-04-08 23:54:28 -0400

    Added initial io stuff
    not yet tested but it should work

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <glog/logging.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 
 int main(int argc, char** argv) {
-  
+	string input; 
+	string output;
+	
+	if (argc > 2) {
+		input = string(argv[1]);
+		output = string(argv[2]);
+	}
+	
+  cv::Mat image = imread(input, CV_LOAD_IMAGE_GREYSCALE); 
+	cv::Mat blue = image(Rect(0, 0, image.cols, image.rows/3));
+	cv::Mat green = image(Rect(0, image.rows/3, image.cols, image.rows/3));
+	cv::Mat red = image(Rect(0, 2 * image.rows/3, image.cols, image.rows/3));
 
 }
