4ad08a57e884ca9c0dde43c78ad8d693ca831526
Mdub28 <weiguang2828@gmail.com>
2014-05-02 12:32:48 -0400

    makefiles

---
+++ CMakeFiles/ColorPlate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ColorPlate.dir/main.cpp.o"
+  "ColorPlate.pdb"
+  "ColorPlate"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ColorPlate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
