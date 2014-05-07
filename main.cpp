f1d9cc5af574e1d694c6288a722c15b7d8acccaa
Mdub28 <weiguang2828@gmail.com>
2014-05-07 00:43:56 -0400

    fixed a bug

--- main.cpp
+++ main.cpp
@@ -29,13 +29,10 @@ void findOffset(Mat blue, Mat green, Mat red, int * offsets) {
 		normalize( filteredR, filteredR, 0, 1, NORM_MINMAX, -1, Mat() );
 		minMaxLoc(filteredG, &gminval, &gmaxval, &gmin, &gmax);
 		minMaxLoc(filteredR, &rminval, &rmaxval, &rmin, &rmax);
-		printf("max: %f, min: %f \n", gmaxval, gminval);
 		offsets[0] = gmax.x - wbor;
 		offsets[1] = gmax.y - hbor;
 		offsets[2] = rmax.x - wbor;
 		offsets[3] = rmax.y - hbor; 
-		printf("offsets:  %d   %d  %d   %d  \n", 
-			offsets[0] , offsets[1] , offsets[2] , offsets[3] );  
 	} else { 
 		Mat smallB, smallG, smallR, redSub, greenSub;
 		pyrDown(blue, smallB);
@@ -49,10 +46,8 @@ void findOffset(Mat blue, Mat green, Mat red, int * offsets) {
 		int wbor = blue.cols / 6;
 		int hbor = blue.rows / 6;
 		Mat bluemid = blue(Rect(wbor, hbor, blue.cols - 2*wbor, blue.rows - 2*hbor));
-		for (int i=-1; i <=1; i++) {
-			for (int j=-1; j<=1; j++) {
-				printf("accessing green with size (%d, %d) at (%d, %d) with size (%d, %d) \n",
-					green.cols, green.rows, wbor+i+2*roffsets[0], hbor+j+2*roffsets[1], blue.cols - 2*wbor, blue.rows - 2*hbor);
+		for (int i=-2; i <=2; i++) {
+			for (int j=-2; j<=2; j++) {
 				Mat greenmid = green(Rect(wbor+i+2*roffsets[0], hbor+j+2*roffsets[1], blue.cols - 2*wbor, blue.rows - 2*hbor));
 				Mat redmid = red(Rect(wbor+i+2*roffsets[2], hbor+j+2*roffsets[3], blue.cols - 2*wbor, blue.rows - 2*hbor));
 				 
@@ -69,6 +64,7 @@ void findOffset(Mat blue, Mat green, Mat red, int * offsets) {
 					offsets[3] = j+2*roffsets[3];
 					rlowest = redSSD;
 				}
+				
 				if (greenSSD <glowest || glowest == -1) {
 					offsets[0] = i+2*roffsets[0];
 					offsets[1] = j+2*roffsets[1];
@@ -100,7 +96,7 @@ Mat shiftImage(int lr, int ud, Mat image) {
 		image= applyTransformation(left_kernel, image, -lr);
 	} else {
 		flip(left_kernel, left_kernel, 1);
-		image = applyTransformation(left_kernel, image, -lr);
+		image = applyTransformation(left_kernel, image, lr);
 	}
 	
 	if (ud > 0) {
@@ -121,7 +117,13 @@ Mat runSequential(Mat image) {
 	Mat red = image(Rect(wbor, 2 * image.rows/3 + hbor, image.cols - 2*wbor, image.rows/3 - 2*hbor));
 	double endTime = CycleTimer::currentSeconds();
 	printf("Sequential trim and separate: %.3f ms %d \n", 1000.f * (endTime-startTime));
-	
+	Mat origmerge;
+	std::vector<Mat> omergevec;
+	omergevec.push_back(blue);
+	omergevec.push_back(green);
+	omergevec.push_back(red);
+	merge(omergevec, origmerge);
+	imwrite("orig.jpg", origmerge);
 	
 	int * offsets = new int[4]();
 	startTime = CycleTimer::currentSeconds();
@@ -132,8 +134,8 @@ Mat runSequential(Mat image) {
 	
 	//shift images by offset
 	startTime = CycleTimer::currentSeconds();
-	green = shiftImage(offsets[0], offsets[1], green);
-	red = shiftImage(offsets[2], offsets[3], red);
+	green = shiftImage(-offsets[0], offsets[1], green);
+	red = shiftImage(-offsets[2], offsets[3], red);
 	endTime = CycleTimer::currentSeconds();
 	printf("Sequential shift: %.3f ms \n", 1000.f * (endTime-startTime));
 	
